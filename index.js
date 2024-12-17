dc3b8f1a5fee0118a10af2d421681a08f6db6851
Tarun <tarun@redblink.com>
2024-12-17 14:35:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import the necessary libraries
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4001;
 
 // Create a new client instance
 const client = new Client({
@@ -35,3 +38,11 @@ client.on('error', (error) => {
 
 // Start the client
 client.initialize();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
